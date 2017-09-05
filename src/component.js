7de15b5edc3f8ece09f90d9d33e37beb172e72bb
angieskazka <konzeptmeister@gmail.com>
2017-09-05 23:52:57 +0200

    OPACITY_SELECT = 1

--- src/component.js
+++ src/component.js
@@ -931,7 +931,7 @@ const BarChartDS = Component.extend("barchartds", {
 
     const OPACITY_HIGHLT = 1.0;
     const OPACITY_HIGHLT_DIM = this.model.marker.opacityHighlightDim;
-    const OPACITY_SELECT = this.model.marker.opacityRegular;
+    const OPACITY_SELECT = 1.0;
     const OPACITY_REGULAR = this.model.marker.opacityRegular;
     const OPACITY_SELECT_DIM = this.model.marker.opacitySelectDim;
 
